6299c2e0644fe957be54e6bd45b324246e7d3d5f
AbubekerMohammed <abumoha1892@gmail.com>
2023-01-04 22:37:04 -0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Getting Started with Admin Dash
+
+This is a React App demonstrating an admin dashboard built to showcase best practices for UI design and development.
+
+###Screenshots
+
+<table>
+<tr>
+  <th>HomePage[Desktop]</th>
+  <th>ChartScreen[Tablet]</th>
+ </tr>
+  <tr>
+    <td> <img src="./screenshots/HomePage[Desktop].png" ></td>
+    <td><img src="./screenshots/CharScreen[Tablet].png" ></td>
+  </tr>
+  <tr>
+    <th>ChartScreen[Desktop]</th>
+  <tr>
+    <td> <img src="./screenshots/CharScreen[Desktop].png" ></td>
+  </tr>
+</table>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "./Sidebar.css";
+import Logo from "../imgs/profile.png";
+import { SidebarData } from "../Data/Data";
+import { UilBars, UilTimes } from "@iconscout/react-unicons";
+import { motion } from "framer-motion";
+
+const Sidebar = () => {
+  const [selected, setSelected] = useState(0);
+
+  const [expanded, setExpaned] = useState(true);
+
+  const sidebarVariants = {
+    true: {
+      left: "0",
+    },
+    false: {
+      left: "-60%",
+    },
+  };
+  return (
+    <>
+      <div
+        className="bars"
+        style={expanded ? { left: "60%" } : { left: "5%" }}
+        onClick={() => setExpaned(!expanded)}
+      >
+        {expanded ? <UilTimes /> : <UilBars />}
+      </div>
+      <motion.div
+        className="sidebar"
+        variants={sidebarVariants}
+        animate={window.innerWidth <= 768 ? `${expanded}` : ""}
+      >
+        {/* logo */}
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+          <span>
+            Admin<span>Dash</span>
+          </span>
+        </div>
+
+        <div className="menu">
+          {SidebarData.map((item, index) => {
+            return (
+              <div
+                className={selected === index ? "menuItem active" : "menuItem"}
+                key={index}
+                onClick={() => setSelected(index)}
+              >
+                <item.icon />
+                <span>{item.heading}</span>
+              </div>
+            );
+          })}
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+export default Sidebar;
